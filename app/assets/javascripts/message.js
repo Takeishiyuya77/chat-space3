d2dff4faa228789018ba9b8c86610eadd066382d
Takeishui_yuya <yuya_dayo_7@icloud.com>
2019-12-05 19:12:18 +0900

    a

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,17 +40,18 @@ $(function(){
    };
  }
 $('.js-form').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+    $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+    })
+});
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);    
@@ -61,6 +62,4 @@ $('.js-form').on('submit', function(e){
     alert('error');
   });
   return false;
-  });
-
 });
